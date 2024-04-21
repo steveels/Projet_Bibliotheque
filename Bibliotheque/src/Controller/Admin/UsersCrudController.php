3dbee26b026b9500b0c6f168bfa4be68b0d0cf9b
Chouanibe97640 <chouanibekamardine@gmail.com>
2024-04-21 15:07:24 +0200

    modification info user #1

--- Bibliotheque/src/Controller/Admin/UsersCrudController.php
+++ Bibliotheque/src/Controller/Admin/UsersCrudController.php
@@ -3,9 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Users;
+use App\Repository\UsersRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
@@ -20,17 +25,31 @@ class UsersCrudController extends AbstractCrudController
     {
         return $crud
             ->setEntityLabelInPlural('Utilisateurs')
-            ->setEntityLabelInSingular('Utilisateurs');
+            ->setEntityLabelInSingular('Utilisateur')
+
+            ->setPageTitle("index", "SymRecipe - Administration des utilisateurs");
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+        return [ 
+            IdField::new('id')
+                ->hideOnForm(),
+            TextField::new('email')
+                ->setFormTypeOption('disabled', 'disabled'),
+            TextField::new('lastname'),
+            TextField::new('firstname'),
+            DateTimeField::new('birthdate'),
+            ArrayField::new('roles'),
+            TextField::new('adress'),
+            TextField::new('city'),
+            TextField::new('zip_code'),
+            IntegerField::new('phoneNumber'),
+            ArrayField::new('reservations'),
+            ArrayField::new('empruntLivres'),
+            ArrayField::new('commentaires'),
+            BooleanField::new('banni')
+            
         ];
     }
-    */
 }
--- Bibliotheque/src/DataFixtures/AppFixtures.php
+++ Bibliotheque/src/DataFixtures/AppFixtures.php
@@ -35,3 +35,4 @@ class AppFixtures extends Fixture
         }
 
 }
+}
